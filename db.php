91f326f8cf9a4faf528978e1fa5533bbee35e15f
Andy-tw-no1 <andy960829@gmail.com>
2026-06-14 19:48:11 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,16 +1,18 @@
 <?php
 
-$conn = new mysqli(
-    "localhost",
-    "root",
-    "",
-    "room"
-);
+$url = getenv("DB_URL");
+
+$db = parse_url($url);
+
+$host = $db["host"];
+$user = $db["user"];
+$pass = $db["pass"];
+$dbname = ltrim($db["path"], "/");
+
+$conn = new mysqli($host, $user, $pass, $dbname, 3306);
 
 if ($conn->connect_error) {
-    die("連線失敗");
+    die("DB connection failed");
 }
-
-echo "資料庫連線成功";
 
 ?>
