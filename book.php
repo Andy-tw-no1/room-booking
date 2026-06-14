6d3ab3a17d9b36e0dc621200da3aa74c3f59482b
Andy-tw-no1 <andy960829@gmail.com>
2026-06-15 01:10:40 +0800

    Update book.php

--- book.php
+++ book.php
@@ -6,7 +6,15 @@ $user = $_POST["user"];
 $date = $_POST["date"];
 $start = $_POST["start"];
 $end = $_POST["end"];
-$sql = $sql = "SELECT * FROM bookings
+
+// 檢查開始時間是否早於結束時間
+if ($start >= $end)
+{
+    die("開始時間必須早於結束時間");
+}
+
+// 檢查是否與既有預約重疊
+$sql = "SELECT * FROM bookings
         WHERE date='$date'
         AND NOT (
             '$end' <= start_time
@@ -15,22 +23,31 @@ $sql = $sql = "SELECT * FROM bookings
 
 $result = $conn->query($sql);
 
-if($result->num_rows > 0)
+if (!$result)
+{
+    die("查詢失敗：" . $conn->error);
+}
+
+if ($result->num_rows > 0)
 {
     die("此時段已被預約");
 }
-$sql = "INSERT INTO bookings
-(user, date, start_time, end_time)
-VALUES
-('$user','$date','$start','$end')";
 
-if($conn->query($sql))
+// 新增預約
+$sql = "INSERT INTO bookings
+        (user, date, start_time, end_time)
+        VALUES
+        ('$user', '$date', '$start', '$end')";
+
+if ($conn->query($sql))
 {
     echo "預約成功";
 }
 else
 {
-    echo "預約失敗";
+    echo "預約失敗：" . $conn->error;
 }
+
+$conn->close();
 
 ?>
