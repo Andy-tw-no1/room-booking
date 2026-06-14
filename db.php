a698f108db84e0d8bd44ebc467e4223e0be83b77
Andy-tw-no1 <andy960829@gmail.com>
2026-06-14 20:16:51 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,17 +1,6 @@
 <?php
 
 $url = getenv("DB_URL");
-$db = parse_url($url);
 
-$host = $db["host"];
-$user = $db["user"];
-$pass = $db["pass"];
-$port = $db["port"] ?? 3306;
-$dbname = ltrim($db["path"], "/");
-
-$conn = new mysqli($host, $user, $pass, $dbname, $port);
-
-if ($conn->connect_error) {
-    die("連線失敗: " . $conn->connect_error);
-}
-?>
+var_dump($url);
+exit;
