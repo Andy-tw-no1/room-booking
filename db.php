9d32bea04f4dce5b4c699febb02a8331dd2fcb70
Andy-tw-no1 <andy960829@gmail.com>
2026-06-14 20:14:16 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,18 +1,17 @@
 <?php
 
 $url = getenv("DB_URL");
-
 $db = parse_url($url);
 
 $host = $db["host"];
 $user = $db["user"];
 $pass = $db["pass"];
+$port = $db["port"] ?? 3306;
 $dbname = ltrim($db["path"], "/");
 
-$conn = new mysqli($host, $user, $pass, $dbname, 3306);
+$conn = new mysqli($host, $user, $pass, $dbname, $port);
 
 if ($conn->connect_error) {
-    die("DB connection failed");
+    die("連線失敗: " . $conn->connect_error);
 }
-
 ?>
