74fb87e329bdc8e4c90ecfe43830a77daa628016
Andy-tw-no1 <andy960829@gmail.com>
2026-06-14 20:25:59 +0800

    Update db.php

--- db.php
+++ db.php
@@ -2,5 +2,21 @@
 
 $url = getenv("DB_URL");
 
-var_dump($url);
-exit;
+$db = parse_url($url);
+
+if (!$db) {
+    die("DB_URL parse failed");
+}
+
+$host = $db["host"];
+$user = $db["user"];
+$pass = $db["pass"];
+$port = $db["port"] ?? 3306;
+$dbname = ltrim($db["path"], "/");
+
+$conn = new mysqli($host, $user, $pass, $dbname, $port);
+
+if ($conn->connect_error) {
+    die("DB connect failed: " . $conn->connect_error);
+}
+?>
