f1818a2387801653f80c31ad0cce7b3cf364d13e
Andy-tw-no1 <andy960829@gmail.com>
2026-06-15 17:41:26 +0800

    Update captcha.php

--- captcha.php
+++ captcha.php
@@ -1,13 +1,12 @@
 <?php
 session_start();
 
-// ⚠️ 只產生一次，避免被 iframe 重刷
-if (!isset($_SESSION["captcha"])) {
-    $_SESSION["captcha"] = rand(1000, 9999);
+$chars = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789";
+$code = "";
+
+for ($i = 0; $i < 4; $i++) {
+    $code .= $chars[rand(0, strlen($chars) - 1)];
 }
 
-// 顯示驗證碼
-echo "<div style='font-size:26px;font-weight:bold;letter-spacing:5px;text-align:center;'>"
-    . $_SESSION["captcha"] .
-"</div>";
+$_SESSION["captcha"] = $code;
 ?>
